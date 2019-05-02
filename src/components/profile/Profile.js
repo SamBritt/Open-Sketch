8af8d8a15f70c94906395ae427efb5414db04af8
SamBritt <sam.britt2@yahoo.com>
2019-05-02 10:31:29 -0500

    click create Canvas button, canvas element renders

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import Canvas from '../canvas/Canvas';
 
 export default class Profile extends Component {
+    state = {
+        showCanvas: false
+    }
+
+    revealCanvas = () => {
+        this.setState({
+            showCanvas: true
+        })
+    }
+
     render() {
         return (
             <React.Fragment>
-            <Canvas />
+                <button onClick = {this.revealCanvas}>Create New Sketch</button>
+                {this.state.showCanvas ? <Canvas /> : null}
             </React.Fragment>
         )
     }
