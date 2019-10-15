d8dbefbb9a1ce1f3c869f43231d4876c79369851
SamBritt <sam.britt2@yahoo.com>
2019-10-15 14:52:11 -0500

    grid react spring added, changed background image

--- src/components/image/ImageModel.js
+++ src/components/image/ImageModel.js
@@ -1,38 +1,13 @@
 import React from 'react';
 
 const ImageModal = (props) => {
-    // state = {
-    //     image: []
-    // }
-
-    // imageToGet = (e) => {
-    //     let image = e.target.src
-    //     return image;
-    // }
-    // toggleThis() {
-    //     this.props.toggleModal();
-    //     this.setState({
-    //         image: this.props.imageClicked
-    //     })
-    // }
-
-    // render() {
-    //     const imageProps = this.props.item;
-    //     let menuVis = this.props.isOpen ? 'is-active' : 'is-disabled'
-    //     console.log(imageProps);
-    //     console.log(this.props);
-
     return (
-        <div key={props.id} id="modalExpand" className={`modal ${props.menuVis} is-clipped`}>
+        <div key={props.id} id="modalExpand" className={`modal ${props.menuVis}`}>
             <div className="modal-background" onClick={props.toggleModal}></div>
             <div className="modal-content">
-                <div class="modal-card">
-                    <section class="modal-card-body">
-                        <figure className="image is-4by4">
+                        <figure className="image is-4by4 is-clipped">
                             <img src={props.imageUrl} alt={props.name}></img>
                         </figure>
-                    </section>
-                </div>
                 <button className="modal-close is-large" aria-label="close" onClick={props.toggleModal}></button>
             </div>
 
--- src/components/profile/CurrentUserProfile.js
+++ src/components/profile/CurrentUserProfile.js
@@ -7,8 +7,9 @@ import '../../../node_modules/d3-ease'
 import { easePoly, easeCubic } from '../../../node_modules/d3-ease';
 import { thisExpression } from '@babel/types';
 import ImageModal from '../image/ImageModel';
-
-
+import { Transition } from 'react-pose';
+import { Grid, Slug, Fade } from 'mauerwerk'
+import Cell from '../image/Cell'
 
 
 export default class CurrentUserProfile extends Component {
@@ -38,19 +39,7 @@ export default class CurrentUserProfile extends Component {
         })
         return fade;
     }
-    toggleModal = (e) => {
-        console.log(e.target)
-        this.setState({
-            imageClicked: e.target.src,
-            isOpen: !this.state.isOpen
-        })
-        console.log(this.state.isOpen)
-    }
-
     render() {
-        let menuVis = this.state.isOpen ? 'is-active' : 'is-disabled'
-        const modalIds = [];
-
         return (
             <React.Fragment>
                 <div className="profileWrapper">
@@ -58,85 +47,24 @@ export default class CurrentUserProfile extends Component {
                     <button className="center-it button is-rounded is-primary" onClick={() => this.props.history.push("/sketch/new")}>Create New Sketch</button>
                 </div>
                 <div className="wrapper">
-                    {/* New Addition */}
+                    <Grid className="grid"
+                        data={this.props.images}
+                        height = {100}
+                        keys={e => e.id}
+                        columns={4}
+                        heights={200}
+                        margin={70}
+                        closeDelay={400}>
+                        {(data, maximized, toggle) => (
+                            <Cell {...data}
+                                history={this.props.history}
+                                handleDelete={this.handleDelete}
+                                maximized={maximized}
+                                toggle={toggle} />
+                        )}
 
-                    {/* End New Addition */}
-
-
-
-                    <Trail items={this.props.images} keys={item => item.id}
-                        config={{ easing: easeCubic, duration: 1200 }}
-                        from={{ opacity: 0, marginTop: 5 }}
-                        to={{ opacity: 1, marginTop: 0 }}>
-                        {item => props =>
-
-                            <div style={props} key={item.id} className="card">
-                                <div className="card-image">
-                                    <figure className="image is-square">
-                                        <img src={item.imageUrl} alt={item.name} onClick={this.toggleModal} />
-                                    </figure>
-
-                                </div>
-                                <div className="card-content">
-                                    <div className="media-content">
-                                        <p className="title is-3">{item.name}</p>
-                                        <p className="subtitle is-6">{item.lessonsLearned}</p>
-                                    </div>
-
-                                    <button className="button is-rounded is-primary" onClick={() => this.props.history.push(`/profile/${item.id}/edit`)}>Edit</button>
-                                    <button className="button is-rounded is-danger" id={item.id} onClick={this.handleDelete}>Delete</button>
-                                    <button className="button is-rounded is-warning" id={item.id} value="button" onClick={this.toggleModal}>Hey</button>
-                                </div>
-
-
-                            </div>
-
-                            
-                            
-                        }
-                        
-                        </Trail>
-                            {
-                                this.state.isOpen ? this.props.images.filter(image => image.imageUrl == this.state.imageClicked ).map(e => 
-                                    <ImageModal 
-                                    name = {e.name} 
-                                    toggleModal = {this.toggleModal} 
-                                    menuVis = {menuVis} 
-                                    skey = {e.id} 
-                                    imageUrl = {e.imageUrl} 
-                                    props = {e}/>
-                                    ) : null
-                            } 
-                            {/*
-                            <ImageModal menuVis={menuVis} name={item.name} imageUrl={item.imageUrl} toggleModal={this.toggleModal} isOpen={this.state.isOpen} />
-                            */}
-                        }
-                        
-                        {/* {this.props.images.map(image =>
-
-                        <div key={image.id} className="card">
-                            <div className="card-image">
-                                <figure className="image is-square">
-                                    <img src={image.imageUrl} alt="Image" />
-                                </figure>
-
-                            </div>
-                            <div className="card-content">
-                                <div className="media-content">
-                                    <p className="title is-3">{image.name}</p>
-                                    <p className="subtitle is-6">{image.lessonsLearned}</p>
-                                </div>
-
-                                <button className="button is-rounded is-primary" onClick={() => this.props.history.push(`/profile/${image.id}/edit`)}>Edit</button>
-                                <button className="button is-rounded is-danger" id={image.id} onClick={this.handleDelete}>Delete</button>
-                            </div>
-                        </div>
-
-
-                        )} */}
+                    </Grid>
                 </div>
-
-
             </React.Fragment >
         )
     }
